939ac28e140f02e10aaa3dfe3a9f7ce46e34ab55
BuzzBeater <baoxiongjia@ucla.edu>
2017-11-02 15:59:45 -0700

    backup

--- kinect/getAlignedSkeletonsWNP.m
+++ kinect/getAlignedSkeletonsWNP.m
@@ -1,4 +1,4 @@
-%   Main logic stream of the get mean
+%%   Main logic stream of the get mean
 clc;
 clear;
 
@@ -13,15 +13,24 @@ root = '../Dataset';
 action2aligned = getAlignedSkeleton(action2anchor, action2skeleton, dir_map);
 save('action2aligned.mat', 'action2aligned');
 
+%%
+action2aligned = load('action2aligned.mat');
+action2aligned = action2aligned.action2aligned;
 
+%%
 action2mean = getMeanSkeletons(action2aligned);
 
+action_index{60} = 'null';
+
+%%
 for action = 1 : size(action2mean, 2)
     if size(action2aligned{action}, 2) ~= 0
         fig = figure();
         plot_skeleton(action2mean{action}', action2skeleton{action}{1}{3}, action2skeleton{action}{1}{2}, dir_map, 'r')
         hold on;
         plot_skeleton(action2aligned{action}{1}', action2skeleton{action}{1}{3}, action2skeleton{action}{1}{2}, dir_map, 'g');
-        title(action_index{action});
+        if action ~= size(action2mean, 2)
+            title(action_index{action});
+        end
     end
 end
